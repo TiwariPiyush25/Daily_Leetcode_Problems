27270dc6cb044d4fa8fe9db4b9ad7ab7bb758059
Piyush Tiwari <piyusht122005@gmail.com>
2025-09-24 15:51:08 +0530

    Time: 185 ms (5.04%), Space: 45.5 MB (84.18%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void swap(int i,int j,int[] arr){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public int[] findErrorNums(int[] arr) {
+        int n=arr.length;
+        int i=0;
+        while(i<n){
+            if(arr[i]==i+1 || arr[i]==arr[arr[i]-1] || arr[i]>n) i++;
+            else swap(i,arr[i]-1,arr);
+        }
+
+        System.out.println(Arrays.toString(arr));
+        int[] ans=new int[2];
+        for(i=0;i<n;i++){
+            if(arr[i]!=i+1) {
+                ans[0]=arr[i];
+                ans[1]=i+1;
+
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+}

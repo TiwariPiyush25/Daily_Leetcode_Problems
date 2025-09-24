bb07f4f225eed5fc22c684fea6bb5453d7bf6d8a
Piyush Tiwari <piyusht122005@gmail.com>
2025-09-24 19:47:21 +0530

    2089-find-target-indices-after-sorting-array.java

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,9 +1,21 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
         List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) ans.add(i);
+        int n=nums.length;
+
+        int smallercount=0; // smaller numbers less than target
+        for(int i=0;i<n;i++){
+            if(nums[i]<target) smallercount++;
+        }
+
+        int targetcount=0; // current occurance of target
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                int idx=smallercount+targetcount;
+
+                ans.add(idx);
+                targetcount++; // target 
+            }
         }
 
         return ans;

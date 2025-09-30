44b1c50a5dc8c478e0141814366d80ba9b151bb9
Piyush Tiwari <piyusht122005@gmail.com>
2025-09-30 22:15:28 +0530

    Time: 52 ms (5.23%), Space: 45.8 MB (12.4%) - LeetHub

--- 1386-shift-2d-grid/1386-shift-2d-grid.java
+++ 1386-shift-2d-grid/1386-shift-2d-grid.java
@@ -1,47 +1,35 @@
 class Solution {
-    public static void reverse(int[] arr,int st,int end){
-        int i=st;
-        int j=end;
-
-        while (i<j){
-            swap(arr,i,j);
-            i++;
-            j--;
-        }
-    }
-    public static void swap(int[] arr,int a,int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
-    }
-    
-    public void rotate(int[] nums, int k) {
-        if(nums.length==1) return;
-        k=k%nums.length;
-        reverse(nums,0,nums.length-k-1);
-        reverse(nums,nums.length-k,nums.length-1);
-        reverse(nums,0,nums.length-1);
-
-    }
-    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int m=grid.length , n=grid[0].length;
-        int[] temp=new int[m*n];
-        int t=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                temp[t++]=grid[i][j];
-            }
-        }
-        rotate(temp,k);
+    public List<List<Integer>> shiftGrid(int[][] arr, int k) {
+        int m=arr.length , n = arr[0].length;
 
         List<List<Integer>> ans=new ArrayList<>();
-        t=0;
         for(int i=0;i<m;i++){
             List<Integer> row=new ArrayList<>();
             for(int j=0;j<n;j++){
-                row.add(temp[t++]);
+                row.add(arr[i][j]);
             }
             ans.add(row);
+        }
+
+        k=k%(m*n);
+        while(k-->0){
+            // change the values
+            for(int i=0;i<m;i++){
+                int temp=arr[i][n-1];
+                for(int j=n-1;j>0;j--){
+                    ans.get(i).set(j,arr[i][j-1]);
+                }
+                int row=(i+1)%m;
+                int col=(n)%n;
+
+                ans.get(row).set(col,temp);
+            }
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    arr[i][j]=ans.get(i).get(j);
+                }
+            }
         }
 
         return ans;

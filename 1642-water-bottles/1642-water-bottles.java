30297e5330a5dbd8554ba73c9b53ec7dddf4fc13
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-01 19:07:43 +0530

    Time: 0 ms (100%), Space: 40.3 MB (86.01%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numWaterBottles(int n, int m) {
+        int total=n;
+
+        while(n>=0){
+            int rem=n%m;
+            if(n<m) break;
+            total+=n/m;
+
+            n=(n/m)+rem;
+        }
+
+        return total;
+    }
+}

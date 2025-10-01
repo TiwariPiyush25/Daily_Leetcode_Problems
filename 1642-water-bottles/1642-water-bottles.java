5e656685d3761b54edf9e3d93c15bf34a61bad1a
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-01 19:09:16 +0530

    Time: 0 ms (100%), Space: 40.3 MB (71.39%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -2,7 +2,7 @@ class Solution {
     public int numWaterBottles(int n, int m) {
         int total=n;
 
-        while(n>=0){
+        while(n>0){
             int rem=n%m;
             if(n<m) break;
             total+=n/m;

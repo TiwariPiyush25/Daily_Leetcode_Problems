b924974fdc3ea5f4957f140f127c7a33737a998e
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-14 11:36:58 +0530

    Time: 8 ms (61.82%), Space: 45.3 MB (14.5%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,25 @@
 class Solution {
-    private int ways(int i, int[] arr, int t) {
+    static int sum=0;
+    private int ways(int i, int[] arr,int res , int t,int[][] dp) {
         if (i==arr.length){
-            if (t==0) return 1; // 1 valid way
+            if (t==res) return 1; // 1 valid way
             else return 0; // Not a valid way
         }
-        int addways = ways(i+1,arr,t-arr[i]);
-        int subways = ways(i+1,arr,t+arr[i]);
+        if (dp[i][res+sum]!=-1) return dp[i][res+sum];
+        int addways = ways(i+1,arr,res+arr[i],t,dp);
+        int subways = ways(i+1,arr,res-arr[i],t,dp);
 
-        return addways + subways;
+        return dp[i][res+sum] = addways + subways;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return ways(0,nums,target);
+        // i = 0 to n-1 | target = -sum to sum
+        // dp[i][t+sum] = ways(i,target)
+        int n=nums.length;
+        sum=0;
+        for (int ele:nums) sum+=ele;
+        int[][] dp=new int[n][sum*2+1];
+        for (int[] a:dp) Arrays.fill(a,-1);
+
+        return ways(0,nums,0,target,dp);
     }
 }

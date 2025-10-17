3e058602cf2a8713ee0838132e0f37147741e943
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-17 12:58:13 +0530

    Time: 1 ms (14.54%), Space: 45.3 MB (25.56%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -0,0 +1,34 @@
+class Solution {
+    public TreeNode helper(TreeNode root1,TreeNode root2){
+        if(root1==null && root2==null) return null;
+        
+        if(root1==null || root2==null) {
+            if(root1==null) 
+            {   
+                root1=root2;
+                return helper(root1,root2);
+            }
+            if(root2==null) {
+                root2=root1;
+                return helper(root1,root2);
+            }
+        }
+        TreeNode root;
+        if(root1!=root2){
+            root = new TreeNode(root1.val+root2.val);
+        }
+        else root=new TreeNode(root1.val);
+
+
+        TreeNode left=helper(root1.left,root2.left);
+        TreeNode right=helper(root1.right,root2.right);
+
+        root.left=left;
+        root.right=right;
+
+        return root;
+    }
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return helper(root1,root2);
+    }
+}

db9a4590acc3cf647119eab236afa2dee15fde1f
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-20 09:24:50 +0530

    Time: 3 ms (5.72%), Space: 43.8 MB (98.7%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Node>> lvlOrder(Node root){
+        List<List<Node>> ans=new ArrayList<>();
+
+        Queue<Node> Q=new LinkedList<>();
+        Q.add(root);
+        while(!Q.isEmpty()){
+            int size=Q.size();
+            List<Node> lst=new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+                Node temp=Q.remove();
+
+                if(temp.left!=null) Q.add(temp.left);
+                if(temp.right!=null) Q.add(temp.right);
+
+                lst.add(temp);
+            }
+
+            ans.add(lst);
+        }
+
+        return ans;
+    }
+    public Node connect(Node root) {
+        if(root==null) return root;
+        List<List<Node>> ans = lvlOrder(root);
+
+        for(List<Node> lst:ans){
+            for(int i=0;i<lst.size()-1;i++){
+                lst.get(i).next=lst.get(i+1);
+            }
+        }
+
+        return root;
+    }
+}

d9c0a3e1220cdff071ba38660f66a64076aac8de
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-23 08:44:57 +0530

    Time: 78 ms (32.16%), Space: 57.3 MB (32.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,34 +1,26 @@
 class Solution {
-    public boolean SubsetExists(int[] arr,int target,int idx,int[][] dp){
-        if (idx==arr.length) return target==0;
-
-        if (dp[idx][target]!=-1) return (dp[idx][target]==1);
-
-        boolean ans=false;
-        boolean skip = SubsetExists(arr,target,idx+1,dp);
-        if (target-arr[idx]<0) return ans = skip; // only valid for +ve Numbers
-        else {
-            boolean pick = SubsetExists(arr,target-arr[idx],idx+1,dp);
-            ans = pick || skip;
-        }
-
-        if (ans) dp[idx][target] = 1;
-        else dp[idx][target] = 0;
-        return ans;
-    }
     public boolean canPartition(int[] arr) {
         int sum=0;
-        for(int ele:arr){
-            sum+=ele;
+        for(int ele:arr) sum+=ele;
+        if(sum%2!=0) return false;
+
+        int target=sum/2;
+        int n=arr.length;
+        int[][] dp=new int[n][target+1];
+
+        for (int i=0;i<n;i++){
+            for (int j=0;j<dp[0].length;j++){
+                boolean ans = false;
+                boolean skip = (i>0) ? (dp[i-1][j]==1) : (j==0);
+                if ( j -arr[i]<0) ans = skip;
+                else {
+                    boolean pick = (i>0) ? (dp[i-1][j-arr[i]]==1) : j==0;
+                    ans = pick || skip;
+                }
+                dp[i][j] = ans ? 1 : 0;
+            }
         }
 
-        if(sum%2!=0) return false;
-        int target=sum/2;
-
-        int n=arr.length;
-        // i = 0 to n-1 | target = target to 0
-        int[][] dp=new int[n][target+1];
-        for (int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        return SubsetExists(arr,target,0,dp);
+        return dp[n-1][target] == 1;
     }
 }

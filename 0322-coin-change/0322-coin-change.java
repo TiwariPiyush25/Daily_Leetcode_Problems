ea47c0d3234590e5d145a0bdb013e11640a6837d
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-23 09:09:32 +0530

    Time: 25 ms (31.75%), Space: 45.6 MB (15.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        long[][] dp=new long[n][amount+1];
+        long[][] dp=new long[2][amount+1];
 
         for (int i=0;i<n;i++){
             for (int j=0;j<dp[0].length;j++){
-                long skip = (i>0) ? dp[i-1][j] : (j==0) ? 0 : Integer.MAX_VALUE;
-                if (j-coins[i]<0) dp[i][j] = skip;
+                long skip = (i>0) ? dp[0][j] : (j==0) ? 0 : Integer.MAX_VALUE;
+                if (j-coins[i]<0) dp[1][j] = skip;
                 else {
-                    long pick = 1 + dp[i][j - coins[i]];
-                    dp[i][j] = Math.min(skip,pick);
+                    long pick = 1 + dp[1][j - coins[i]];
+                    dp[1][j] = Math.min(skip,pick);
                 }
             }
+
+            // copy 1st row in 0th row
+            for(int j=0;j<dp[0].length;j++){
+                dp[0][j] = dp[1][j];
+            }
         }
-        int ans=(int) dp[n-1][amount];
+        int ans=(int) dp[1][amount];
         if (ans==Integer.MAX_VALUE) return -1;
         return ans;
     }

27276daa463e89851306f09bb602edc7df1d0e93
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-23 09:01:52 +0530

    Time: 26 ms (30.01%), Space: 45.4 MB (21.8%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,19 @@
 class Solution {
-    public long coincount(int[] arr,int amount,int i,long[][] dp){
-        if (i==arr.length){
-            if (amount==0) return 0; //  no more coins needed
-            else return Integer.MAX_VALUE; //  not a valid Combination
-        }
-
-        if (dp[i][amount]!=-1) return dp[i][amount];
-        long skip = coincount(arr,amount,i+1,dp) ;
-        if (amount-arr[i]<0) return dp[i][amount] = skip;
-        long pick = 1 + coincount(arr,amount-arr[i],i,dp); // no. of coins
-
-        return dp[i][amount] = Math.min(skip,pick);
-    }
     public int coinChange(int[] coins, int amount) {
-        // i = 0  to n-1 | amount = amount to 0
         int n=coins.length;
         long[][] dp=new long[n][amount+1];
-        for (int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        int ans=(int) coincount(coins,amount,0,dp);
 
+        for (int i=0;i<n;i++){
+            for (int j=0;j<dp[0].length;j++){
+                long skip = (i>0) ? dp[i-1][j] : (j==0) ? 0 : Integer.MAX_VALUE;
+                if (j-coins[i]<0) dp[i][j] = skip;
+                else {
+                    long pick = 1 + dp[i][j - coins[i]];
+                    dp[i][j] = Math.min(skip,pick);
+                }
+            }
+        }
+        int ans=(int) dp[n-1][amount];
         if (ans==Integer.MAX_VALUE) return -1;
         return ans;
     }

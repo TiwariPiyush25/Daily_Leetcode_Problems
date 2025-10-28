05db8c9869f99af474867c6d47bef07fc1d51548
Piyush Tiwari <piyusht122005@gmail.com>
2025-10-28 22:00:55 +0530

    Time: 4 ms (4.2%), Space: 44.7 MB (6.29%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int lcs(int i,int j,String a,String b,int[][] dp){
+        if (i<0 || j<0) return 0;
+
+        if (dp[i][j]!=-1) return dp[i][j];
+        if (a.charAt(i)==b.charAt(j)) return dp[i][j] = 1 + lcs(i-1,j-1,a,b,dp);
+        else {
+            return dp[i][j] = Math.max(lcs(i,j-1,a,b,dp),lcs(i-1,j,a,b,dp));
+        }
+    }
+    public static int longestCommonSubsequence(String a,String b){
+        int m = a.length() , n = b.length();
+        int[][] dp=new int[m][n];
+        for (int i=0;i<dp.length;i++) Arrays.fill(dp[i],-1);
+
+        return lcs(m-1,n-1,a,b,dp);
+    }
+    public boolean isSubsequence(String s, String t) {
+        int ans = longestCommonSubsequence(s,t);
+
+        return ans == s.length();
+    }
+}

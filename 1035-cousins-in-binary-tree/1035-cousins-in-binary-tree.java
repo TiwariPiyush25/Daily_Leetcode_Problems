4d4b50c3f862a23bc7b4bceec3c8443e77650e6c
Piyush Tiwari <piyusht122005@gmail.com>
2025-11-11 22:21:42 +0530

    Time: 1 ms (22.62%), Space: 43.4 MB (7.89%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -1,21 +1,12 @@
-class triplet{
-    int val;
-    int parent;
-    int l;
-
-    triplet(int v,int p,int l){
-        this.val =v;
-        this.parent = p;
-        this.l = l;
-    }
-}
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
         Queue<TreeNode> q = new LinkedList<>();
-        ArrayList<triplet> arr=new ArrayList<>();
 
         q.add(root);
         int lev = 0 ;
+
+        int p1 = -1 , p2 = -1;
+        int l1 = -1 , l2 = -1;
 
         while(!q.isEmpty()){
             int size = q.size();
@@ -24,33 +15,33 @@ class Solution {
                 TreeNode temp=q.remove();
 
                 if(temp.left!=null){
-                    arr.add(new triplet(temp.left.val,temp.val,lev));
+                    if(temp.left.val == x){
+                        p1 = temp.val;
+                        l1 = lev;
+                    }
+                    else if(temp.left.val == y){
+                        p2 = temp.val;
+                        l2 = lev;
+                    }
                     q.add(temp.left);
                 }
 
                 if(temp.right!=null){
-                    arr.add(new triplet(temp.right.val,temp.val,lev));
+                    if(temp.right.val == x){
+                        p1 = temp.val;
+                        l1 = lev;
+                    }
+                    else if(temp.right.val == y){
+                        p2 = temp.val;
+                        l2 = lev;
+                    }
+
                     q.add(temp.right);
                 }
             }
             lev++;
         }
 
-        int p1 = -1;
-        int p2 = -1;
-        int l1 = -1;
-        int l2 = -1;
-
-        for(triplet t:arr){
-            if(t.val==x){
-                p1 = t.parent;
-                l1 = t.l;
-            }
-            if(t.val == y){
-                p2 = t.parent;
-                l2 = t.l;
-            }
-        }
         return p1!=p2 && l1==l2;
     }
 }

4b86fc5aaf72885d2d338ebc01ef160ab4d72c47
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-13 19:22:16 +0530

    Time: 57 ms (5.06%), Space: 47 MB (16.4%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,48 @@
+class pair implements Comparable<pair>{
+    int value;
+    int count;
+
+    pair(int v,int c){
+        this.value = v;
+        this.count = c;
+    }
+
+    public int compareTo(pair p){
+        if(p.count == this.count) return this.value - p.value;
+        return this.count - p.count;
+    }
+}
+class Solution {
+    public int count1s(int x){
+        String binary = "";
+        int count = 0;
+        while(x>1){
+            int ld = x % 2;
+            if(ld==1) count++;
+            binary = ld + binary;
+            x/=2;
+        }
+
+        if(x==1) {
+            binary = x + binary;
+            count++;
+        }
+        System.out.println(binary);
+        return count;
+    }
+    public int[] sortByBits(int[] arr) {
+        PriorityQueue<pair> heap = new PriorityQueue<>();
+
+        for(int ele:arr){
+            heap.add(new pair(ele,count1s(ele)));
+        }
+
+        int i=0;
+        while(!heap.isEmpty()){
+            pair p = heap.remove();
+            arr[i++] = p.value;
+        }
+
+        return arr;
+    }
+}

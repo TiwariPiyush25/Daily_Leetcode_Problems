657de937e271894f1d5c5305f9d6d2f32c9c696a
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-22 13:05:08 +0530

    Time: 43 ms (45.31%), Space: 46.2 MB (45.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,18 @@
 class Solution {
-    public int binarySearch(int[] arr,int st,int end,int target){
-        while (st<end){
-            int mid = st+ (end-st)/2;
-
-            if (arr[mid] == target) return mid;
-            else if (arr[mid] >  target) end = mid;
-            else st = mid+1;
-        }
-
-        return st;
-    }
     public int lengthOfLIS(int[] arr) {
         int n = arr.length;
         int[] dp = new int[n];
 
         int max = 0;
-        for (int i=0;i<n;i++){
-            // find the index of height of arr[i]
-            int index = binarySearch(dp,0,max,arr[i]);
-
-            dp[index] = arr[i];
-
-            if (index == max){
-                max++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[i],dp[j]);
+                }
             }
+            dp[i] +=1;
+
+            max = Math.max(max,dp[i]);
         }
 
         return max;

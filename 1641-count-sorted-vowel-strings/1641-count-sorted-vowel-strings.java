46f35c383069cb05fbf68b5d0fc70c9f88975e70
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-23 22:07:41 +0530

    Time: 4 ms (14.97%), Space: 42.6 MB (6.89%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int helper(int i,int n,String vowel,String s,int[][] dp){
+        if(i>=vowel.length()) return 0;
+        if(n==0) return 1;
+        
+        if(dp[n][i]!=-1) return dp[n][i];
+        int skip = 0 , take = 0;
+        skip += helper(i+1,n,vowel,s,dp);
+        take += helper(i,n-1,vowel,s+vowel.charAt(i),dp);
+
+        return dp[n][i] = skip + take;
+    }
+    public int countVowelStrings(int n) {
+        int[][] dp = new int[n+1][5];
+        for(int[] arr:dp) Arrays.fill(arr,-1);
+
+        return helper(0,n,"aeiou","",dp);
+    }
+}

12b03dbfe0ddf3692caebca91d6985b99f42e3e7
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-26 20:22:24 +0530

    Time: 9 ms (94.58%), Space: 47.1 MB (10.77%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,31 +1,11 @@
 class Solution {
-    public  class pair implements Comparable<pair>{
-        int noOfBoxes;
-        int UnitesperBox;
-
-        pair(int n,int u){
-            this.noOfBoxes = n;
-            this.UnitesperBox = u;
-        }
-
-        public int compareTo(pair p){
-            return p.UnitesperBox - this.UnitesperBox;
-        }
-    }
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        pair[] arr = new pair[boxTypes.length];
-
-        int i = 0;
-        for (int[] a:boxTypes){
-            arr[i++] = new pair(a[0],a[1]);
-        }
-
-        Arrays.sort(arr);
+        Arrays.sort(boxTypes,(a,b) -> b[1] - a[1]); // custom sort
 
         int result = 0;
-        for (pair p:arr){
-            int n = p.noOfBoxes;
-            int u = p.UnitesperBox;
+        for (int[] arr:boxTypes){
+            int n = arr[0];
+            int u = arr[1];
 
             int min = Math.min(truckSize,n);
             truckSize -= min;

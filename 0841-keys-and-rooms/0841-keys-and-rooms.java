98494b097695852ae7d5ce7d6970c98b9d402202
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-29 20:53:42 +0530

    Time: 2 ms (58.39%), Space: 46.7 MB (11.71%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void bfs(int i,List<List<Integer>> adj,boolean[] vis){
+        int size = adj.get(i).size();
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(i);
+
+        while(!Q.isEmpty()){
+            int temp = Q.remove();
+
+            for(int ele:adj.get(temp)){
+                if(!vis[ele]){
+                    Q.add(ele);
+                    vis[ele] = true;
+                }
+            }
+        }
+    }
+    public boolean canVisitAllRooms(List<List<Integer>> adj) {
+        int n = adj.size();
+        boolean[] vis = new boolean[n];
+
+        vis[0] = true;
+        bfs(0,adj,vis);
+        for(boolean ele:vis) if(!ele) return ele;
+        return true;
+    }
+}

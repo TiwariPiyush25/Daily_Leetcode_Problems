f8862db5f0d8cbe9124ee02cec0c2f0ff2c5f5fd
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-30 12:34:38 +0530

    Time: 5 ms (32.75%), Space: 51.6 MB (81.15%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,55 @@
+class Solution {
+    public class pair{
+        int row;
+        int col;
+
+        pair(int r,int c){
+            this.row = r;
+            this.col = c;
+        }
+    }
+    public void bfs(int i,int j,char[][] grid,boolean[][] vis){
+        Queue<pair> q = new LinkedList<>();
+        q.add(new pair(i,j));
+
+        while(!q.isEmpty()){
+            pair p = q.remove();
+            int r = p.row;
+            int c = p.col;
+
+            if(r>0 && !vis[r-1][c] &&  grid[r-1][c] == '1'){
+                vis[r-1][c] = true;
+                q.add(new pair(r-1,c));
+            }
+            if(c>0 && !vis[r][c-1] &&  grid[r][c-1] == '1'){
+                vis[r][c-1] = true;
+                q.add(new pair(r,c-1));
+            }
+            if(r<grid.length-1 && !vis[r+1][c] &&  grid[r+1][c] == '1'){
+                vis[r+1][c] = true;
+                q.add(new pair(r+1,c));
+            }
+            if(c<grid[0].length-1 && !vis[r][c+1] && grid[r][c+1] == '1'){
+                vis[r][c+1] = true;
+                q.add(new pair(r,c+1));
+            }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        boolean[][] vis = new boolean[m][n];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    vis[i][j] = true;
+                    bfs(i,j,grid,vis);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}

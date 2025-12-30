31713f204f5fb8bb34c649a1a7010d5aa899d0e1
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-30 19:59:18 +0530

    Time: 15 ms (60.53%), Space: 51.8 MB (5.94%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int count = 0;
+        int j = 0;
+        for(int i = 0;i<g.length;i++){
+            while(j<s.length && s[j]<g[i]) j++;
+            if(j == s.length) break;
+
+            if(j<s.length && s[j]>=g[i]) {
+                count++;
+                j++;
+            }
+        }
+
+        return count;
+    }
+}

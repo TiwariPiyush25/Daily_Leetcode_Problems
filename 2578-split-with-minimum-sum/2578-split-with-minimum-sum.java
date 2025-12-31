adcaca398cd20ab4a79d8e7ffb42c7eb2b7b1eee
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-31 23:24:14 +0530

    Time: 1 ms (93.37%), Space: 42.2 MB (52.71%) - LeetHub

---
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int splitNum(int num) {
+        List<Integer> list = new ArrayList<>();
+
+        while(num > 0){
+            list.add(num % 10);
+            num/=10;
+        }
+
+        Collections.sort(list);
+        int num1 = 0;
+        int num2 = 0;
+
+        for(int i=0;i<list.size();i++){
+            if(i%2==0) num1 = num1*10 + list.get(i);
+            else  num2 = num2 * 10 + list.get(i);
+        }
+
+        return num1 + num2;
+    }
+}

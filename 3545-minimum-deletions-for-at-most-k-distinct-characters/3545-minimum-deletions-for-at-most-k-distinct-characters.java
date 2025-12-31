cc4f7d6a91855e4df30082a465ff6b40187dfe8e
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-31 23:06:11 +0530

    Time: 10 ms (6.9%), Space: 44.5 MB (7.28%) - LeetHub

---
+++ 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDeletion(String s, int k) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0) + 1);
+        }
+
+        int[] arr = new int[map.size()];
+        int i= 0;
+        for(var key:map.keySet()){
+            arr[i++] = map.get(key);
+        }
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int total = 0;
+        for(i=0;i<arr.length - k;i++){ 
+            total += arr[i];
+        }
+
+        return total;
+    }
+}

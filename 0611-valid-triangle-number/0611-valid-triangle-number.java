c2108974fc8fccc48fc2d02b2038b9a11ae10967
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-04 19:58:26 +0530

    Time: 27 ms (92.28%), Space: 45.8 MB (19.28%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,26 +1,16 @@
 class Solution {
-    public int Search(int l,int r,int target,int[] arr){
-        int ans=r+1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(arr[mid]>=target){
-                r=mid-1;
-                ans=mid;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
-    }
-    public int triangleNumber(int[] nums) {
-        Arrays.sort(nums);
+    public int triangleNumber(int[] arr) {
+        Arrays.sort(arr);
         int count = 0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int sum = nums[i] + nums[j];
-                int idx = Search(j+1,nums.length-1,sum,nums);
-                count += (idx-j-1);
+        for(int k = arr.length - 1;k>=0;k--){
+            int i = 0;
+            int j = k-1;
+            while(i<j){
+                if(arr[i] + arr[j] > arr[k]){
+                    count += (j - i);
+                    j--;
+                }
+                else i++;
             }
         }
 

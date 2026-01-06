9016cac8676763b26665f9e93db7674943f7c4de
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-06 22:14:21 +0530

    Time: 6 ms (18.01%), Space: 42.6 MB (78.48%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,25 +1,18 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int n = 1 << nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
 
-        for(int i=0;i<n;i++){
-            List<Integer> temp = new ArrayList<>();
-
-            for(int j = 0;j<nums.length;j++){
-                int mask = 1 << j;
-
-                if((i & mask)!=0) temp.add(nums[j]);
-            }
-            ans.add(temp);
-        }
-
+        int total = 1 << nums.length;
         int sum = 0;
-        for(List<Integer> temp:ans){
-            int xor = 0;
-            for(int ele:temp) xor ^= ele;
 
-            sum+=xor;
+        for(int num=0;num<total;num++){
+            int xor = 0;
+            for(int bitidx=0;bitidx<n;bitidx++){
+                int mask = 1 << bitidx;
+
+                if((num & mask)!=0) xor ^= nums[bitidx];
+            }
+            sum += xor;
         }
 
         return sum;

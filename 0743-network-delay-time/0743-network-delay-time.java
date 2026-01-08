f66ee277d3322e158250a62c9bf78961b070e6b9
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-08 20:00:32 +0530

    Time: 9 ms (95.17%), Space: 48.9 MB (76.66%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -13,24 +13,25 @@ class Solution {
         }
     }
     public int networkDelayTime(int[][] times, int n, int src) {
+        // create adj List
         List<List<pair>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<n+1;i++) adj.add(new ArrayList<>());
 
         for(int[] arr:times){
-            int node1 = arr[0] - 1;
-            int node2 = arr[1] - 1;
+            int node1 = arr[0];
+            int node2 = arr[1];
             int time = arr[2];
 
             adj.get(node1).add(new pair(node2,time));
         }
 
         // Apply Dijkstra Algorithm
-        int[] time = new int[n];
+        int[] time = new int[n+1];
         Arrays.fill(time,Integer.MAX_VALUE);
-        time[src-1] = 0;
+        time[src] = 0;
 
         PriorityQueue<pair> pq = new PriorityQueue<>();
-        pq.add(new pair(src-1,0));
+        pq.add(new pair(src,0));
 
         while(!pq.isEmpty()){
             pair top = pq.remove();
@@ -44,9 +45,10 @@ class Solution {
                 }
             }
         }
-
+        // find maximum time 
         int max = Integer.MIN_VALUE;
-        for(int ele:time){
+        for(int i=1;i<time.length;i++){
+            int ele = time[i];
             max = Math.max(max,ele);
         }
 

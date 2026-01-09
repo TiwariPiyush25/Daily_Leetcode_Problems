db9dbed7a798ad70b6cfdea53168880ae4631584
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-09 11:35:20 +0530

    Time: 42 ms (57.37%), Space: 48.4 MB (6.46%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -27,44 +27,24 @@ class Solution {
 
         PriorityQueue<Triplet> pq = new PriorityQueue<>();
         pq.add(new Triplet(0,0,0));
-
+        int[] r = {-1,0,1,0}; // row
+        int[] c = {0,-1,0,1}; // col
         while(!pq.isEmpty()){
             Triplet top = pq.remove();
             int row = top.row , col = top.col, effort = top.effort;
 
             if(row == m-1 && col == n-1) break;
             if(effort > ans[row][col]) continue;
-            
-            if(row > 0){ // going up row-1
-                int e = Math.abs(arr[row-1][col] - arr[row][col]);
+
+            for(int i = 0;i<=3;i++){
+                int newRow = row + r[i];
+                int newCol = col + c[i];
+                if(newRow <0 || newCol <0 || newRow > m-1 || newCol > n-1) continue;
+                int e = Math.abs(arr[newRow][newCol] - arr[row][col]);
                 e = Math.max(e,effort);
-                if(e < ans[row-1][col]){
-                    ans[row-1][col] = e;
-                    pq.add(new Triplet(row-1,col,e));
-                }
-            } 
-            if(col > 0){ // going left col-1
-                int e = Math.abs(arr[row][col-1] - arr[row][col]);
-                e = Math.max(e,effort);
-                if(e < ans[row][col-1]){
-                    ans[row][col-1] = e;
-                    pq.add(new Triplet(row,col-1,e));
-                }
-            }
-            if(row < m-1){ // going down row-1
-                int e = Math.abs(arr[row+1][col] - arr[row][col]);
-                e = Math.max(e,effort);
-                if(e < ans[row+1][col]){
-                    ans[row+1][col] = e;
-                    pq.add(new Triplet(row+1,col,e));
-                }
-            } 
-            if(col < n-1){ // going right col-1
-                int e = Math.abs(arr[row][col+1] - arr[row][col]);
-                e = Math.max(e,effort);
-                if(e < ans[row][col+1]){
-                    ans[row][col+1] = e;
-                    pq.add(new Triplet(row,col+1,e));
+                if(e < ans[newRow][newCol]){
+                    ans[newRow][newCol] = e;
+                    pq.add(new Triplet(newRow,newCol,e));
                 }
             }
         }

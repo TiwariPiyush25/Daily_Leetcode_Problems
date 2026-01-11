6caf0e8a30fb33215d8cb69d29bf748a11ae37a6
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-11 13:28:05 +0530

    Time: 12 ms (60.13%), Space: 48.8 MB (84.03%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,47 +1,15 @@
 class Solution {
-    public class pair implements Comparable<pair>{
-        int node;
-        int time;
-
-        pair(int n,int t){
-            this.node = n;
-            this.time = t;
-        }
-
-        public int compareTo(pair p){
-            return this.time - p.time;
-        }
-    }
     public int networkDelayTime(int[][] times, int n, int src) {
-        // create adj List
-        List<List<pair>> adj = new ArrayList<>();
-        for(int i=0;i<n+1;i++) adj.add(new ArrayList<>());
-
-        for(int[] arr:times){
-            int node1 = arr[0];
-            int node2 = arr[1];
-            int time = arr[2];
-
-            adj.get(node1).add(new pair(node2,time));
-        }
-
-        // Apply Dijkstra Algorithm
         int[] time = new int[n+1];
         Arrays.fill(time,Integer.MAX_VALUE);
         time[src] = 0;
 
-        PriorityQueue<pair> pq = new PriorityQueue<>();
-        pq.add(new pair(src,0));
+        for(int iteration=1;iteration<=n-1;iteration++){ // n-1 time relaxation
+            for(int[] t:times){
+                int u = t[0],v = t[1],wt = t[2];
 
-        while(!pq.isEmpty()){
-            pair top = pq.remove();
-
-            if(top.time > time[top.node]) continue;
-            for(pair p:adj.get(top.node)){
-                int totaltime = top.time + p.time;
-                if(totaltime < time[p.node]){
-                    time[p.node] = totaltime;
-                    pq.add(new pair(p.node,totaltime));
+                if(time[u]!=Integer.MAX_VALUE && time[u] + wt < time[v]){ // relaxation
+                    time[v] = time[u] + wt;
                 }
             }
         }

c1a9b629bae60eced917f134ac0417cba50fa153
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-12 20:01:26 +0530

    Time: 2 ms (48%), Space: 47.2 MB (64.2%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,39 @@
 class Solution {
-    public void dfs(int i,boolean[] vis,int[][] adj){
-        int n  = adj.length;
-        vis[i] = true;
+    static int[] parent;
+    public int find(int a){
+        if(parent[a]==a) return a;
 
-        for(int j = 0;j<n;j++){
-            if(adj[i][j] == 1 && !vis[j]){
-                dfs(j,vis,adj);
-            }
+        return find(parent[a]); // check leader of parent of a..
+    }
+    public void union(int a,int b){
+        int leaderA = find(a);
+        int leaderB = find(b);
+        
+        if(leaderA != leaderB) {
+            parent[leaderB] = leaderA;
         }
     }
-    public int findCircleNum(int[][] adj) {
-        int n = adj.length;
-        int count = 0;
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
 
-        boolean[] vis = new boolean[n];
-        for (int i = 0;i<n;i++){
-            if (!vis[i]) {
-                dfs(i,vis,adj);
-                count++;
+        parent = new int[n+1]; // 1 to n nodes
+        parent[0] = Integer.MIN_VALUE; // does not exist node
+        for(int i = 1;i<=n;i++) parent[i] = i;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i == j) continue;
+                // edge is from i+1 to j+1
+                if(isConnected[i][j]==1) union(i+1,j+1); // connecting group leaders of i+1 & j+1
             }
         }
 
-        return count;
+
+        int ConnectedComponents = 0;
+        for(int i=1;i<=n;i++){
+            if(parent[i] == i) ConnectedComponents++;
+        }
+
+        return ConnectedComponents;
     }
 }

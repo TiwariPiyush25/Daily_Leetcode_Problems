b9a5f169653a8e42dda842bfafe941d13b1db0a9
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-13 19:46:14 +0530

    Time: 2 ms (39.44%), Space: 47.4 MB (23.52%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,40 +1,49 @@
 class Solution {
-    static boolean ans;
-    public void bfs(int i,int[][] adj,int[] vis){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        // Color new component...
-        vis[i] = 0; // 1 --> Red && 0 --> Blue
-        while(!q.isEmpty()){
-            int temp = q.remove();
+    public int[] parent,size;
+    public boolean[] parity;
+    public int leader(int u){
+        if(parent[u]==u) return u;
+        
+        return parent[u] = leader(parent[u]);
+    }
+    public void union(int u,int v){
+        int a = leader(u);
+        int b = leader(v);
 
-            int color = vis[temp];
-            for(int ele:adj[temp]){
-                if(vis[ele] == vis[temp]){
-                    ans = false;
-                    return;
-                }
-
-                if(vis[ele]==-1){
-                    q.add(ele);
-                    vis[ele] = 1 - color;
-                }
+        if(a != b){
+            if(size[a]  >size[b]){
+                parent[b] = a;
+                size[a] += size[b];
+                parity[v] = !parity[u];
+            }
+            else{
+                parent[a] = b;
+                size[b] += size[a];
+                parity[u] = !parity[v];
             }
         }
     }
     public boolean isBipartite(int[][] graph) {
-        ans = true;
         int n = graph.length;
-        int[] visited = new int[n];
-        Arrays.fill(visited,-1);
-        
+
+        parent = new int[n];
+        size = new int[n];
+        parity = new boolean[n];
+
         for(int i=0;i<n;i++){
-            if(visited[i]==-1){
-                if(ans == false) return ans;
-                bfs(i,graph,visited);
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        for(int i =0;i<graph.length;i++){
+            for(int j=0;j<graph[i].length;j++){
+                if(graph[i][j] >  i){
+                    if(leader(i) == leader(graph[i][j]) && parity[i] == parity[graph[i][j]]) return false;
+                    else union(i,graph[i][j]);
+                }
             }
         }
 
-        return ans;
+        return true;
     }
 }

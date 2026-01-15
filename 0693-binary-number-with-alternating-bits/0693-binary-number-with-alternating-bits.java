f89c88d057df55c61240450c8b7181efe81f349d
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-15 13:16:01 +0530

    Time: 6 ms (0.81%), Space: 42.3 MB (16.79%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -2,13 +2,12 @@ class Solution {
     public boolean hasAlternatingBits(int n) {
         boolean[] isSet = new boolean[32];
 
-        for(int i = 0;i<=31;i++){
+        for(int i = 0;i<=31 && (1 << i) < n;i++){
             int mask = 1 << i;
 
             if((n & (mask)) !=0) isSet[i] = true;
             System.out.println(mask);
-            if(i>0 && ((isSet[i-1] && isSet[i]) || (!isSet[i-1] && !isSet[i])) && mask < n) return false;
-            if(mask >= n) break;
+            if(i>0 && ((isSet[i-1] && isSet[i]) || (!isSet[i-1] && !isSet[i]))) return false;
         }
 
         return true;

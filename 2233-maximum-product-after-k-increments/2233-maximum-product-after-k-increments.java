6944fc07f88b011f632f0187b5a734e9c456d2e3
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-21 20:06:46 +0530

    Time: 394 ms (5.4%), Space: 100.2 MB (5.71%) - LeetHub

---
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumProduct(int[] nums, int k) {
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+
+        for(int ele:nums){
+            minheap.add(ele);
+        }
+
+        while(k-->0){
+            int min = minheap.remove();
+            min  += 1;
+            minheap.add(min);
+        }
+
+        System.out.println(minheap);
+        long product = 1;
+        while(!minheap.isEmpty()){
+            product = (product * minheap.remove()) % 1000000007;
+        }
+
+        return (int) product;
+    }
+}

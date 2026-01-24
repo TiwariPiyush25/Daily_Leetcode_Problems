4c8cf3c3191521baffda54974b722025c413965c
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-24 22:52:36 +0530

    Time: 328 ms (5.03%), Space: 51.3 MB (68.34%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,46 @@
+class MyHashMap {
+    ArrayList<Integer> arr1;
+    ArrayList<Integer> arr2;
+    public MyHashMap() {
+        arr1 =  new ArrayList<>();
+        arr2 =  new ArrayList<>();
+    }
+    
+    public void put(int key, int value) {
+        if(arr1.contains(key)){
+            int idx = arr1.indexOf(key);
+
+            arr2.set(idx,value);
+        }
+        else {
+            arr1.add(key);
+            arr2.add(value);
+        }
+    }
+    
+    public int get(int key) {
+        if(arr1.contains(key)){
+            int idx = arr1.indexOf(key);
+
+            return arr2.get(idx);
+        }
+        else return -1;
+    }
+    
+    public void remove(int key) {
+        if(arr1.contains(key)){
+            int idx = arr1.indexOf(key);
+
+            arr1.remove(idx);
+            arr2.remove(idx);
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */

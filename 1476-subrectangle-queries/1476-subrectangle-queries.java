7be05447aab71b3625236c6758793e5c7f6ddd3d
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-29 22:47:29 +0530

    Time: 23 ms (17.31%), Space: 48.7 MB (86.81%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.java
+++ 1476-subrectangle-queries/1476-subrectangle-queries.java
@@ -2,7 +2,14 @@ class SubrectangleQueries {
     int[][] mat;
 
     public SubrectangleQueries(int[][] rectangle) {
-        mat = rectangle;
+        int m = rectangle.length , n = rectangle[0].length;
+        mat = new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = rectangle[i][j];
+            }
+        }
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {

0fcf6aca7dff65b5fca6035536b958c8a55494ab
Piyush Tiwari <piyusht122005@gmail.com>
2026-02-09 21:58:04 +0530

    Time: 4 ms (64.01%), Space: 45.3 MB (61.59%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,11 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       int[] arr = new int[32];
-
        int res = 0; 
        for(int bit=0;bit<32;bit++){
             int count =  0;
-            
             for(int ele:nums){
                 if((ele & (1<<bit))!= 0) count++;
             }

0db423f6316306238aa562b63bb732aa2600e72d
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-09 22:10:37 +0530

    Time: 1 ms (88.24%), Space: 47.5 MB (42.8%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,50 +1,32 @@
 class Solution {
-    public int find(int a,int[] parent){
-        if(parent[a]==a) return a;
-
-        return find(parent[a],parent); // check leader of parent of a..
-    }
-    public void union(int a,int b,int[] parent,int[] size){
-        a = find(a,parent);
-        b = find(b,parent);
+    public void bfs(int i,int[][] isConnected,boolean[] vis){
+        vis[i] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
         
-        if(a != b) {
-            if(size[a]>size[b]){ // a should b eparent of b
-                parent[b] = a;
-                size[a] += size[b]; 
-            }
-            else {
-                parent[a] = b;
-                size[b] += size[a]; 
+        while(!q.isEmpty()){
+            int top = q.remove();
+
+            for(int j=0;j<isConnected.length;j++){
+                if(!vis[j] && isConnected[top][j] == 1){
+                    vis[j] = true;
+                    q.add(j);
+                }
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
+        boolean[] vis = new boolean[n];
 
-        int[] parent = new int[n+1]; // 1 to n nodes
-        int[] size = new int[n+1];
-        parent[0] = Integer.MIN_VALUE; // does not exist node
-
-        for(int i = 1;i<=n;i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
-
+        int count = 0; // count provinces
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i == j) continue;
-                // edge is from i+1 to j+1
-                if(isConnected[i][j]==1) union(i+1,j+1,parent,size); // connecting group leaders of i+1 & j+1
+            if(!vis[i]) {
+                bfs(i,isConnected,vis);
+                count++;
             }
         }
 
-
-        int ConnectedComponents = 0;
-        for(int i=1;i<=n;i++){
-            if(parent[i] == i) ConnectedComponents++;
-        }
-
-        return ConnectedComponents;
+        return count;
     }
 }

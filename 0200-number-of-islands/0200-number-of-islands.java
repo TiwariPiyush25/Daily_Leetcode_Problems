97ddf5285872f2aef74b6de4434e4f81fc45da68
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-09 22:47:57 +0530

    Time: 5 ms (30.24%), Space: 53.9 MB (11.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,4 +1,8 @@
 class Solution {
+    public boolean isValid(int newRow,int newCol,int m,int n,char[][] grid,boolean[][] vis){
+        return newRow>=0 && newRow<m && newCol>=0 && newCol<n && 
+              !vis[newRow][newCol] && grid[newRow][newCol] == '1';
+    }
     public void dfs(int i,int j,char[][] grid,boolean[][] vis){
         int m = grid.length, n = grid[0].length;
         vis[i][j] = true;
@@ -10,7 +14,7 @@ class Solution {
             int newRow = i + row[k];
             int newCol = j + col[k];
 
-            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !vis[newRow][newCol] && grid[newRow][newCol] == '1'){
+            if(isValid(newRow,newCol,m,n,grid,vis)){
                 dfs(newRow,newCol,grid,vis);
             }
         }

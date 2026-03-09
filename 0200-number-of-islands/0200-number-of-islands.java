f74a0097c23beb333ba2af098e7d08ebc5ad190a
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-09 22:38:12 +0530

    Time: 5 ms (30.24%), Space: 54.7 MB (6.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,33 @@
 class Solution {
-    public class pair{
-        int row;
-        int col;
-
-        pair(int r,int c){
-            this.row = r;
-            this.col = c;
-        }
-    }
     public void dfs(int i,int j,char[][] grid,boolean[][] vis){
+        int m = grid.length, n = grid[0].length;
         vis[i][j] = true;
 
-        if(i>0 && !vis[i-1][j] &&  grid[i-1][j] == '1') dfs(i-1,j,grid,vis);
-        if(i<grid.length-1 && !vis[i+1][j] &&  grid[i+1][j] == '1') dfs(i+1,j,grid,vis);
-        if(j>0 && !vis[i][j-1] &&  grid[i][j-1] == '1') dfs(i,j-1,grid,vis);
-        if(j<grid[0].length-1 && !vis[i][j+1] &&  grid[i][j+1] == '1') dfs(i,j+1,grid,vis);
+        int[] row = {-1,0,1,0};
+        int[] col = {0,-1,0,1};
+
+        for(int k=0;k<4;k++){
+            int newRow = i + row[k];
+            int newCol = j + col[k];
+
+            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !vis[newRow][newCol] && grid[newRow][newCol] == '1'){
+                dfs(newRow,newCol,grid,vis);
+            }
+        }
     }
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
+        int islands = 0;
+        int m = grid.length, n = grid[0].length;
         boolean[][] vis = new boolean[m][n];
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
-                    vis[i][j] = true;
                     dfs(i,j,grid,vis);
-                    count++;
+                    islands++;
                 }
             }
         }
-
-        return count;
+        
+        return islands;
     }
 }

1a6009296ccd7b079fec20f26b8031dd8da62e92
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-12 22:59:15 +0530

    Time: 8 ms (52.81%), Space: 78 MB (5.52%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        int n = s.length();
+        long[] arr = new long[n];
+        arr[n-1] = shifts[n-1];
+        for(int i = n-2;i>=0;i--){
+            arr[i] = shifts[i] + arr[i+1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            int pos = (int) s.charAt(i) - 97;
+            char ch = (char) (((long)(pos + arr[i]) % 26) + 97);
+
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+}

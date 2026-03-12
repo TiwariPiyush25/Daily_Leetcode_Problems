a6a51ad4062e58e0496169ff80256f15eef46f85
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-12 15:39:41 +0530

    Time: 0 ms (100%), Space: 47 MB (6.03%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -1,21 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    static int maxSum;
-    public int lineSum(TreeNode root){
-        if(root==null) return 0;
-        int leftlineSum = lineSum(root.left);
-        int rightlineSum = lineSum(root.right);
+    public int maxSum;
+    public int pathSum(TreeNode root){
+        if(root == null) return 0;
 
-        int pathSum = root.val;
-        if(leftlineSum>0) pathSum += leftlineSum;
-        if(rightlineSum>0) pathSum += rightlineSum;
-        maxSum = Math.max(maxSum,pathSum);
+        int pathsum = root.val;
+        int leftpathSum = pathSum(root.left);
+        int rightpathSum = pathSum(root.right);
+        if(leftpathSum > 0) pathsum += leftpathSum;
+        if(rightpathSum > 0) pathsum += rightpathSum;
+        maxSum = Math.max(maxSum , pathsum);
 
-        return root.val + Math.max(0,Math.max(leftlineSum,rightlineSum));
+        return root.val + Math.max(0,Math.max(leftpathSum,rightpathSum));
     }
     public int maxPathSum(TreeNode root) {
         maxSum = Integer.MIN_VALUE;
-        lineSum(root);
-
+        pathSum(root);
         return maxSum;
     }
 }

c2bcf28fddabe424031e13d55f066f9841e3f00f
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-12 19:58:35 +0530

    Time: 1 ms (100%), Space: 46.3 MB (14.29%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String s) {
+        int i = 0;
+        int j = 0;
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int cnt = 0;
+        while(j<s.length()){
+            if(s.charAt(i) == s.charAt(j)){
+                cnt++;
+            }
+            else{
+                if(cnt >= 3){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(i);
+                    temp.add(j-1);
+
+                    ans.add(temp);
+                }
+                i = j;
+                cnt = 1;
+            }
+
+            j++;
+        }
+        
+        if(cnt >= 3){
+            List<Integer> temp = new ArrayList<>();
+            temp.add(i);
+            temp.add(j-1);
+
+             ans.add(temp);
+        }
+
+        return ans;
+    }
+}

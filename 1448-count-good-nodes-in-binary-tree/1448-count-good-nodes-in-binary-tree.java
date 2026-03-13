261e6e179a0dc737593f1701ca3410031125c215
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-13 16:28:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -1,20 +1,21 @@
 class Solution {
     public int cnt;
-    public List<Integer> maxValueList;
+    public int maxcurr;
+    public int maxprev;
     public void inorder(TreeNode root){
         if(root == null) return;
 
-        int val = maxValueList.get(maxValueList.size()-1);
-        maxValueList.add(Math.max(val,root.val));
-        if(val <= root.val) cnt++;
+        maxprev = maxcurr;
+        maxcurr = Math.max(maxcurr,root.val);
+        if(maxcurr <= root.val) cnt++;
         inorder(root.left);
         inorder(root.right);
-
-        maxValueList.remove(maxValueList.size()-1);
+        maxcurr = maxprev;
     }
     public int goodNodes(TreeNode root) {
-        maxValueList = new ArrayList<>();
-        maxValueList.add(root.val);
+        maxcurr = root.val;
+        maxprev = 0;
+    
         cnt = 0;
         inorder(root);
         return cnt;

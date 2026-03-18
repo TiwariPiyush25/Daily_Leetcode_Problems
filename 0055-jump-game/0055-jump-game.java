65429117581e66b0989d2399a479bf8248a0d71f
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-18 11:15:50 +0530

    Time: 2 ms (85.43%), Space: 47.9 MB (76.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,15 @@
 class Solution {
-    public boolean Jump(int idx,int[] arr,HashMap<Integer,Boolean> dp){
-        if(idx >= arr.length) return false;
-        if(idx == arr.length-1) return true;
-        
-        if(dp.containsKey(idx)) return dp.get(idx);
-        boolean flag = false;
-        for(int i=arr[idx];i>=1;i--){
-            flag = Jump(idx + i,arr,dp);
-            if(flag) {
-                dp.put(idx+i,flag);
-                return true;
+    public boolean canJump(int[] nums) {
+        int farthestReach = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i > farthestReach){
+                return false;
             }
+            
+            int CurrmaxJump = i + nums[i];
+            farthestReach = Math.max(farthestReach,CurrmaxJump);
         }
 
-        dp.put(idx,flag);
-        return flag;
-    }
-    public boolean canJump(int[] nums) {
-        HashMap<Integer,Boolean> dp = new HashMap<>();
-        return Jump(0,nums,dp);
+        return true;
     }
 }

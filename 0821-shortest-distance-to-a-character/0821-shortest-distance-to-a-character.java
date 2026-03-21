305c3627102f00d465324d933748cc7304b69fed
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-21 21:53:11 +0530

    Time: 2 ms (45.31%), Space: 44 MB (62.49%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -1,16 +1,31 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
-        int[] ans = new int[s.length()];
-        Arrays.fill(ans,Integer.MAX_VALUE);
+        int n = s.length();
+        int[] ans = new int[n];
+        
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int idx = Integer.MAX_VALUE;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == c) idx = i;
+            prefix[i] = idx;
+        }
+
+        idx = Integer.MAX_VALUE;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i) == c) idx = i;
+            suffix[i] = idx;
+        }
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == c) {
                 ans[i] = 0;
                 continue;
             }
-            for(int j=0;j<s.length();j++){
-                if(s.charAt(j) == c){
-                    ans[i] = Math.min(ans[i],Math.abs(i-j));
-                }
+            else {
+                int x1 = Math.abs(i-prefix[i]);
+                int x2 = Math.abs(i-suffix[i]);
+
+                ans[i] = Math.min(x1,x2);
             }
         }
 

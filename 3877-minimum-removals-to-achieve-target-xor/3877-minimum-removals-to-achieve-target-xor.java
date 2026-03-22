fa6c01ff0b420ce7c387df8b2b2ee5152950f033
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-22 21:31:15 +0530

    Time: 734 ms (25.43%), Space: 161 MB (8.24%) - LeetHub

--- 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.java
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int helper(int i,int l,int xor,int[] arr,int t,HashMap<String,Integer> dp){
+    public int helper(int i,int xor,int[] arr,int t,HashMap<String,Integer> dp){
         if(i==arr.length){
             if(xor == t){
                 return 0;
@@ -8,16 +8,15 @@ class Solution {
         }
         String key = i+","+xor;
         if(dp.containsKey(key)) return dp.get(key);
-        int take = helper(i+1,l+1,xor ^ arr[i],arr,t,dp);
-        if(take != Integer.MIN_VALUE) take = 1 + take;
-        int skip = helper(i+1,l,xor,arr,t,dp);
+        int take = 1 + helper(i+1,xor ^ arr[i],arr,t,dp);
+        int skip = helper(i+1,xor,arr,t,dp);
 
         dp.put(i+","+xor,Math.max(take,skip));
         return Math.max(take,skip);
     }
     public int minRemovals(int[] nums, int target) {
         HashMap<String,Integer> dp = new HashMap<>();
-        int maxlen = helper(0,0,0,nums,target,dp);
+        int maxlen = helper(0,0,nums,target,dp);
 
         if(maxlen < 0) return -1;
         return nums.length - maxlen;

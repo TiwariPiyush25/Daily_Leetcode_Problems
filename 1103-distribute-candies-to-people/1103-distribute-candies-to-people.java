24189f4c0b9534f1835d5e1fd1525c42f1d0f741
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-24 21:43:54 +0530

    Time: 2 ms (91.2%), Space: 42.5 MB (67.6%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] distributeCandies(int c, int n) {
+        int i = 0;
+        int[] ans = new int[n];
+        while(c > 0){
+            int idx = i % n;
+
+            if(c >= i+1){
+               ans[idx] += i+1;
+               c -= i+1;     
+            }
+            else {
+                ans[idx] += c;
+                break;
+            }
+
+            i++;
+        }
+
+        return ans;
+    }
+}

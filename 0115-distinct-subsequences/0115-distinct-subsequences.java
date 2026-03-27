10ec3d6481e33064d164af1fe634ebfe6f68c4d4
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-27 20:10:35 +0530

    Time: 149 ms (5.13%), Space: 61.2 MB (9.22%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,9 +2,8 @@ class Solution {
     public int helper(int i,String temp,String s,String t,int[][] dp){
         int len = temp.length();
         if(temp.equals(t)) return 1;
-        if((len > 0 && temp.charAt(len-1) != t.charAt(len-1)) || i >= s.length() || temp.length() >= t.length()){
-            return 0;
-        }
+        if((len > 0 && temp.charAt(len-1) != t.charAt(len-1)) || i >= s.length() || temp.length() >= t.length()) return 0;
+        
 
         if(dp[i][temp.length()] != -1) return dp[i][temp.length()];
 
@@ -14,7 +13,6 @@ class Solution {
         return dp[i][temp.length()] = skip + take;
     }
     public int numDistinct(String s, String t) {
-        if(s.equals(t)) return 1;
         int[][] dp = new int[s.length()][t.length()];
         for(int[] d:dp) Arrays.fill(d,-1);
         return helper(0,"",s,t,dp);

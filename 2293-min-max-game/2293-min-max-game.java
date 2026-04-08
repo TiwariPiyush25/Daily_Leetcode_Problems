b713b858b2a751fde3b3a143bed7b6594c6849b8
Piyush Tiwari <piyusht122005@gmail.com>
2026-04-08 20:08:48 +0530

    Time: 1 ms (68.23%), Space: 45.2 MB (38.72%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -4,13 +4,9 @@ class Solution {
         while(size > 1){
             int i = 0 , j = 0;
             while(i < size){
-                if(j % 2 == 0){
-                    nums[j] = Math.min(nums[i],nums[i+1]);
-                }
-                else {
-                    nums[j] = Math.max(nums[i],nums[i+1]);
-                }
-
+                if(j % 2 == 0) nums[j] = Math.min(nums[i],nums[i+1]);
+                else nums[j] = Math.max(nums[i],nums[i+1]);
+                
                 i += 2; j++;
             }
             size /= 2;

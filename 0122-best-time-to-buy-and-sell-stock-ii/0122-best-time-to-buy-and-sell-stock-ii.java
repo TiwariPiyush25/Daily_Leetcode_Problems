1ef0f50bdc9f0897cb136a7a22ef686f125991bb
Piyush Tiwari <piyusht122005@gmail.com>
2026-04-14 21:50:40 +0530

    Time: 0 ms (100%), Space: 46.6 MB (19.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,33 +2,18 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int[] next = new int[2];
-        int[] curr = new int[2];
-
-        next[0] = next[1] = 0;
+        int nextNotBuy = 0,  nextBuy = 0;
+        int currNotBuy = 0, currBuy = 0;
 
         for(int i=n-1;i>=0;i--){
-            for(int Canbuy=0;Canbuy<=1;Canbuy++){
-                int profit = 0;
+            currNotBuy = Math.max(prices[i] + currBuy,currNotBuy);
+            currBuy = Math.max(-prices[i] + currNotBuy,currBuy);
 
-                if(Canbuy == 1){
-                    int take = -prices[i] + next[0];
-                    int nottake =  next[1];
-                    profit = Math.max(take,nottake);
-                }
-                else{
-                    int sell = prices[i] + next[1];
-                    int notsell = next[0];
-                    profit = Math.max(sell,notsell);
-                }
 
-                curr[Canbuy]  = profit;
-            }
-
-            next[0] = curr[0];
-            next[1] = curr[1];
+            nextNotBuy = currNotBuy;
+            nextBuy = currBuy;
         }
 
-        return next[1];
+        return nextBuy;
     }
 }

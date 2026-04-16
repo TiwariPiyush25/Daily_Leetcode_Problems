6e650719661d64131bc99cd2a8390fac71ad2d59
Piyush Tiwari <piyusht122005@gmail.com>
2026-04-16 20:51:53 +0530

    Time: 12 ms (81.53%), Space: 42.3 MB (65.32%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -2,9 +2,9 @@ class Solution {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long cnt = 0;
 
-        for(int sum = 0;sum <= total;sum += cost1){
-            cnt += (total - sum) / cost2;
-            cnt++; // for zero every time
+        for(int sum = 0;sum <= total;sum += cost1){ // pens
+            cnt++; // for zeroth pencil every time
+            cnt += (total - sum) / cost2; // pencils
         }
 
         return cnt;

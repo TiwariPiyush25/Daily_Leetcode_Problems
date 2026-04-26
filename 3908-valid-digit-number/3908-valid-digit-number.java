8559955d293509b84178f222e89deef158f677c9
Piyush Tiwari <piyusht122005@gmail.com>
2026-04-26 12:40:05 +0530

    Time: 2 ms (0%), Space: 42.8 MB (0%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.java
+++ 3908-valid-digit-number/3908-valid-digit-number.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean validDigit(int n, int x) {
         String s = "" + n;
         int idx = s.indexOf(""+x);
-        System.out.println(idx);
         return idx == 0 || idx == -1 ? false : true;
     }
 }

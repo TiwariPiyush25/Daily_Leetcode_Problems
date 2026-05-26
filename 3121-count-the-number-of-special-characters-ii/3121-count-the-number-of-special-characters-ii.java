980d2d04155578fab477164d2c0e2506a3110571
Piyush Tiwari <piyusht122005@gmail.com>
2026-05-26 14:41:47 +0530

    Time: 36 ms (40.08%), Space: 47.8 MB (91.9%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();
 
         for(int i = 0;i < word.length();i++) {
             char ch = word.charAt(i);
 
-            if(ch >= 'A' && ch <= 'Z' && !map1.containsKey(ch)) map1.put(ch,i);
-            else if(ch >= 'a' && ch <= 'z') map2.put(ch,i);
+            if(ch >= 'A' && ch <= 'Z' && !map.containsKey(ch)) map.put(ch,i);
+            else if(ch >= 'a' && ch <= 'z') map.put(ch,i);
         }
         int count = 0;
-        for(char key : map1.keySet()){
-            char target = (char)((int) key + 32);
-            if(map2.containsKey(target) && map2.get(target) < map1.get(key)) count++; 
+        for(char key : map.keySet()){
+            if(key >= 'A' && key <= 'Z') {
+                char target = (char)((int) key + 32);
+                if(map.containsKey(target) && map.get(target) < map.get(key)) count++;
+            } 
         }
 
         return count;

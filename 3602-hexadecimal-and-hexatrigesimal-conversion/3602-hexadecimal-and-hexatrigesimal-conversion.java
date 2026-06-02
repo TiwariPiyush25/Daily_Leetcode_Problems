72df554575fdf8c3ccd2f3a81d587d1e6768cca7
Piyush Tiwari <piyusht122005@gmail.com>
2026-06-02 21:18:01 +0530

    Time: 6 ms (22.94%), Space: 44 MB (24.12%) - LeetHub

--- 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.java
+++ 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.java
@@ -1,25 +1,12 @@
 class Solution {
     public String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public String hexaDecimal(int n){
+    public String convert(int n,int base){
         String ans = "";
 
-        while(n >= 16){
-            int rem = n % 16;
+        while(n >= base){
+            int rem = n % base;
             ans = chars.charAt(rem) + ans;
-            n /= 16;
-        }
-
-        if(n > 0) ans = chars.charAt(n) + ans;
-
-        return ans;
-    }
-    public String hexatrigesimal(int n){
-        String ans = "";
-
-        while(n >= 36){
-            int rem = n % 36;
-            ans = chars.charAt(rem) + ans;
-            n /= 36;
+            n /= base;
         }
 
         if(n > 0) ans = chars.charAt(n) + ans;
@@ -27,8 +14,8 @@ class Solution {
         return ans;
     }
     public String concatHex36(int n) {
-        String s = hexaDecimal(n * n);
-        String t = hexatrigesimal(n * n * n);
+        String s = convert(n * n , 16);
+        String t = convert(n * n * n , 36);
         
         return s + t;
     }

35ff1eaefd3082238a90f221db0126b4eb5b481a
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-03 10:34:42 +0530

    Time: 0 ms (100%), Space: 9.4 MB (5.92%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    int nCr(int n,int r){
-        long long ans = 1;
-
-        int m = min(n-r,r);
-
-        for(int i = 1;i <= m;i++){
-            ans *= (n-i+1);
-            ans /= i;
-        }
-
-        return (int) ans;
-    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> vec(numRows);
 
         for(int i = 0;i < numRows;i++){
             for(int j = 0; j <= i;j++){
-                int val = nCr(i,j);
-
-                vec[i].push_back(val);
+                if(j == 0 || j == i) vec[i].push_back(1);
+                else {
+                    int val = vec[i-1][j] + vec[i-1][j-1];
+                    vec[i].push_back(val);
+                }
             }
         }
 

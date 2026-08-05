98cf447bf52338edf578303dfe7199d836141d20
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-05 14:17:00 +0530

    Time: 440 ms (25.52%), Space: 363.7 MB (19.66%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void bfs(int node,vector<vector<int>> &adj,vector<bool> &vis){
+        vis[node] = 1;
+
+        queue<int> q;
+        q.push(node);
+
+        while(not q.empty()){
+            int front = q.front();
+            
+            for(int ele : adj[front]){
+                if(not vis[ele]){
+                    q.push(ele);
+                    vis[ele] = 1;
+                }
+            }
+            q.pop();
+        }
+
+    }
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+
+        for(vector<int> inv : invocations){
+            int u = inv[0] , v = inv[1];
+
+            adj[u].push_back(v);
+        }
+
+        vector<bool> suspicious(n);
+        bfs(k , adj, suspicious);
+
+        bool flag = 0;
+        for(vector<int> inv : invocations){
+            int u = inv[0] , v = inv[1];
+
+            if(not suspicious[u] && suspicious[v]) {
+                flag = 1;
+                break;
+            }
+        }
+
+        vector<int> ans;
+        for(int i = 0;i < n;i++){
+            if(flag) ans.push_back(i);
+            else if(not suspicious[i]) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};

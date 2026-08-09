e54245077ce322dff491da58964304df8b1d1010
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-09 20:53:47 +0530

    Time: 0 ms (100%), Space: 42.7 MB (72.73%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Set<Character> set = new HashSet<>();
+        
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+            if(set.contains(ch)) return ch;
+
+            set.add(ch);
+        }
+
+        return 'a';
+    }
+}

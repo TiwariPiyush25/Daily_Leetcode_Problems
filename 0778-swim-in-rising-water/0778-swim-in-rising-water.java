b2f8af6bfbee565f62265ac54ee8d4ca1e00dca3
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-14 13:09:21 +0530

    Time: 11 ms (62.85%), Space: 46.2 MB (77.33%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,15 +1,14 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int m = grid.length , n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
 
         int[][] dir = {{-1,0},{0,-1},{0,1},{1,0}};
-
-        int[][] Time = new int[m][n];
-        for(int[] t : Time) Arrays.fill(t,Integer.MAX_VALUE);
+        int[][] time = new int[m][n];
+        for(int[] t : time) Arrays.fill(t , Integer.MAX_VALUE);
 
         PriorityQueue<int[]> minheap = new PriorityQueue<>((a,b) -> a[2] - b[2]);
-        Time[0][0] = grid[0][0];
         minheap.add(new int[]{0,0,grid[0][0]});
+        time[0][0] = grid[0][0];
 
         while(!minheap.isEmpty()){
             int[] top = minheap.remove();
@@ -17,18 +16,18 @@ class Solution {
 
             if(r == m-1 && c == n-1) return t;
 
-            if(Time[r][c] < t) continue;
+            if(time[r][c] < t) continue;
 
             for(int i = 0;i < 4;i++){
-                int nr = r + dir[i][0], nc = c + dir[i][1];
+                int nr = r + dir[i][0] , nc = c + dir[i][1];
 
                 if(nr >= 0 && nr <= m-1 && nc >= 0 && nc <= n-1){
                     int nt = Math.max(t , grid[nr][nc]);
 
-                    if(nt >= Time[nr][nc]) continue;
+                    if(nt >= time[nr][nc]) continue;
 
                     minheap.add(new int[]{nr,nc,nt});
-                    Time[nr][nc] = nt;
+                    time[nr][nc] = nt;
                 }
             }
         }

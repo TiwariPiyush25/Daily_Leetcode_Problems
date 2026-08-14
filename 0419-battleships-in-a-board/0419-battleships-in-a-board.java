b8cd1e715bbb43ebb705d171762b27c0afd98556
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-14 09:32:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -5,9 +5,7 @@ class Solution {
         int m = board.length , n = board[0].length;
         for(int i = 0;i < board.length;i++){
             for(int j = 0;j < board[0].length;j++){
-                if(board[i][j] == 'X') counter++;
-                if(i > 0 && board[i][j] == 'X' && board[i-1][j] == 'X') counter--;
-                if(j > 0 && board[i][j] == 'X' && board[i][j-1] == 'X') counter--;
+                if(board[i][j] == 'X' && (i == 0 || board[i-1][j] != 'X') && (j == 0 || board[i][j-1] != 'X')) counter++;
             }
         }
 

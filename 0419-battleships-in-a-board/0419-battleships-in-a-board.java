95f2f19bf4bc78f7b6626130420307b9de3430d8
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-14 09:28:09 +0530

    Time: 2 ms (7.31%), Space: 45.2 MB (96.72%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,20 +1,13 @@
 class Solution {
-    public void dfs(int i,int j,char[][] arr){
-        arr[i][j] = '.';
-
-        if(i+1 < arr.length && arr[i+1][j] == 'X') dfs(i+1,j,arr); // down
-        if(j+1 < arr[0].length && arr[i][j+1] == 'X') dfs(i,j+1,arr); // right
-    }
     public int countBattleships(char[][] board) {
         int counter = 0;
 
         int m = board.length , n = board[0].length;
         for(int i = 0;i < board.length;i++){
             for(int j = 0;j < board[0].length;j++){
-                if(board[i][j] == 'X'){
-                    counter++;
-                    dfs(i,j,board);
-                }
+                if(board[i][j] == 'X') counter++;
+                if(i > 0 && board[i][j] == 'X' && board[i-1][j] == 'X') counter--;
+                if(j > 0 && board[i][j] == 'X' && board[i][j-1] == 'X') counter--;
             }
         }
 

e5a62b9fed03fb23724c9cf5a93d642941fa6b15
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-14 09:24:52 +0530

    Time: 1 ms (98.82%), Space: 45.7 MB (38.6%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,21 +1,19 @@
 class Solution {
-    public void dfs(int i,int j,char[][] arr,boolean[][] vis){
-        vis[i][j] = true;
+    public void dfs(int i,int j,char[][] arr){
+        arr[i][j] = '.';
 
-        if(i+1 < arr.length && !vis[i+1][j] && arr[i+1][j] == 'X') dfs(i+1,j,arr,vis); // down
-        if(j+1 < arr[0].length && !vis[i][j+1] && arr[i][j+1] == 'X') dfs(i,j+1,arr,vis); // right
+        if(i+1 < arr.length && arr[i+1][j] == 'X') dfs(i+1,j,arr); // down
+        if(j+1 < arr[0].length && arr[i][j+1] == 'X') dfs(i,j+1,arr); // right
     }
     public int countBattleships(char[][] board) {
         int counter = 0;
 
         int m = board.length , n = board[0].length;
-        boolean[][] vis = new boolean[m][n];
-
         for(int i = 0;i < board.length;i++){
             for(int j = 0;j < board[0].length;j++){
-                if(!vis[i][j] && board[i][j] == 'X'){
+                if(board[i][j] == 'X'){
                     counter++;
-                    dfs(i,j,board,vis);
+                    dfs(i,j,board);
                 }
             }
         }

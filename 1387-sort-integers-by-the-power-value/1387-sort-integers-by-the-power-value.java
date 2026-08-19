69f410d2cc874e2321df515eb0c057e3b6776182
Piyush Tiwari <piyusht122005@gmail.com>
2026-08-19 21:35:31 +0530

    Time: 52 ms (30.82%), Space: 45 MB (93.05%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getPower(int x){
+        if(x == 1) return 0;
+
+        int even = 0, odd = 0;
+        if(x % 2 == 0){
+            even = 1 + getPower(x/2);
+        }
+        if(x % 2 != 0){
+            odd = 1 + getPower((3 * x) + 1);
+        }
+
+        return even + odd;
+    }
+    public int getKth(int lo, int hi, int k) {
+        PriorityQueue<int[]> maxheap = new PriorityQueue<>((a,b) -> (a[1] == b[1]) ? b[0] - a[0] : b[1] - a[1]);
+        for(int i = lo;i <= hi;i++){
+            int pow = getPower(i);
+            maxheap.add(new int[]{i,pow});
+
+            if(maxheap.size() > k) maxheap.remove();
+        }
+
+        return maxheap.peek()[0];
+    }
+}

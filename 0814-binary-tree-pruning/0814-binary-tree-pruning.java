e52c3ea0a6fd30b41e33d492e708eea522ee13a7
Piyush Tiwari <piyusht122005@gmail.com>
2026-09-10 09:50:15 +0530

    Time: 0 ms (100%), Space: 43.1 MB (27.35%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.java
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -1,10 +1,7 @@
 
 class Solution {
     public int sum(TreeNode root){
-        return (root == null) ? 0 : (root.val + sum(root.left) + sum(root.right));
-    }
-    public void helper(TreeNode root){
-        if(root == null) return;
+        if(root == null) return 0;
 
         int leftSum = sum(root.left);
         int rightSum = sum(root.right);
@@ -12,13 +9,10 @@ class Solution {
         if(leftSum == 0) root.left = null;
         if(rightSum == 0) root.right = null;
 
-        helper(root.left);
-        helper(root.right);
+        return root.val + leftSum + rightSum;
     }
     public TreeNode pruneTree(TreeNode root) {
-        if(sum(root) == 0) return null;
-        
-        helper(root);
-        return root;
+        int ans = sum(root);   
+        return ans == 0 ? null : root;
     }
 }

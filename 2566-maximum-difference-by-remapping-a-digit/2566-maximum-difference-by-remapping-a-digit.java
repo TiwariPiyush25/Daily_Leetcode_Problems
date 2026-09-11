87c369d849ddc99c845d6592e2bfee4df1674056
Piyush Tiwari <piyusht122005@gmail.com>
2026-09-11 12:02:12 +0530

    Time: 6 ms (3.68%), Space: 42.5 MB (28.83%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
@@ -8,7 +8,7 @@ class Solution {
         int i = 0;
         while(i < s.length() && s.charAt(i) == '9') i++;
         if(i == s.length()) return num;
-        
+
         for(int j = 0;j < s.length();j++){
             if(s.charAt(j) == s.charAt(i)) val1 += '9';
             else val1 += s.charAt(j);
@@ -17,6 +17,6 @@ class Solution {
             else val2 += s.charAt(j);
         }
 
-        return Integer.parseInt(val1) - Integer.parseInt(val2);
+        return Integer.valueOf(val1) - Integer.valueOf(val2);
     }
 }
